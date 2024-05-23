0e667b2b1840b2f4ac1ff5f603f96f1593af08ea
H153165g <chrn22018@g.nihon-u.ac.jp>
2024-05-23 17:26:24 +0900

    未完成

--- src/Chart.jsx
+++ src/Chart.jsx
@@ -1,104 +1,172 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import * as d3 from "d3";
 
-const COLO=(col,item,color)=>{
-    for(let i=0;i<col.length;i++){
-        if(item==col[i]){
-            console.log(col[i],color(i))
-            return color(i);
+const COLO = (col, item, color) => {
+    for (let i = 0; i < col.length; i++) {
+        if (item == col[i]) {
+            return color[i];
         }
     }
-        
+
 }
-export default function Chart (data1) {
+
+export default function Chart(data1) {
+
+    const [data,setData]=useState(data1["data"])
+    const [dataM, setData1] = useState(data)
+    const types = Object.getOwnPropertyNames(dataM[0])
+    types.pop()
+    const [typex, setTypeX] = useState(types[0])
+    const [typey, setTypeY] = useState(types[1])
 
 
-    const data=data1["data"]
-    console.log(data)
+    const handleChangex = (e) => {
+        setTypeX(e.target.value)
+    }
+
+    const handleChangey = (e) => {
+        setTypeY(e.target.value)
+    }
+
+    function handleClick(i, count) {
+        setCount(count.map((item, j) => {
+            if (i == j) {
+                if (item) {
+                    return false;
+                } else {
+                    setData1(data)
+                    console.log("A")
+                    return true;
+                }
+            } else {
+                return count[j]
+            }
+
+        }));
+
+
+        for(let j=0;j<count.length;j++){
+            console.log(count[j])
+            if (count[j]==false) {
+                console.log(dataM.filter((item) => item.species != col[j]))
+            }
+        }
+        
+    }
+
+
+
+
+
 
     const width = 500
     const height = 500
+
+    const colormod = d3.scaleOrdinal(d3.schemeCategory10)
+    const colors = new Set(data.map(({ species }) => species))
+    const col = Array.from(colors)
+    const [count, setCount] = useState(Array.from({ length: col.length }).map(() => true))
     
-    const color = d3.scaleOrdinal(d3.schemeCategory10)
-    const colors=new Set(data.map(({species})=>species))
-    const col=Array.from(colors)
+    console.log(count)
+    const color = Array.from({ length: col.length }).map((_, i) => {
+        return colormod(i)
+    })
     const xmin = d3.min(data.map((data) => {
-        return (data["sepalLength"])
+        return (data[typex])
     }))
     const ymin = d3.min(data.map((data) => {
-        return (data["sepalWidth"])
+        return (data[typey])
     }))
     const xmax = d3.max(data.map((data) => {
-        return (data["sepalLength"])
+        return (data[typex])
     }))
     const ymax = d3.max(data.map((data) => {
-        return (data["sepalWidth"])
+        return (data[typey])
     }))
 
     const xScale = d3.scaleLinear()
-        .domain([Math.floor(xmin),  Math.floor(xmax)+1])
-        .range([0, width-40])
+        .domain([Math.floor(xmin), Math.floor(xmax) + 1])
+        .range([0, width - 40])
         .nice()
     const yScale = d3.scaleLinear()
-        .domain([Math.floor(ymin), Math.floor(ymax)+1])
-        .range([0, height-40])
+        .domain([Math.floor(ymin), Math.floor(ymax) + 1])
+        .range([0, height - 40])
         .nice()
-    const X=Array.from({length: (Math.floor(xmax)+1-Math.floor(xmin))*2+1}).map((_,i)=>{
-        return i/2+Math.floor(xmin);
-    })  
-    const Y=Array.from({length: (Math.floor(ymax)+1-Math.floor(ymin))*5+1}).map((_,i)=>{
-        return i/5+Math.floor(ymin);
-    })  
-    
-    console.log((Math.floor(xmax)+1-Math.floor(xmin))*2)    
+    const X = Array.from({ length: (Math.floor(xmax) + 1 - Math.floor(xmin)) * 2 + 1 }).map((_, i) => {
+        return i / 2 + Math.floor(xmin);
+    })
+    const Y = Array.from({ length: (Math.floor(ymax) + 1 - Math.floor(ymin)) * 5 + 1 }).map((_, i) => {
+        return i / 5 + Math.floor(ymin);
+    })
 
-   
 
-    console.log(X)
 
-    
-    
-    return (<svg width={width+5} height={height+5}>
-        {X.map((item) => {
-            console.log("A")
-            return (
-                <>
-                    <line x1={xScale(item)+40} y1={height-40} x2={xScale(item)+40} y2={height - 30} stroke="black" />
-                    <text
-                        x={xScale(item)+40}
-                        y={height - 20}
-                        fontSize="10"
-                        textAnchor="middle"
-                        fill="black"
-                        strokeWidth="3">{item}</text>
-                </>
-            )
-        })}
-        {Y.map((item) => {
-            return (
-                <>
-                    <line x1={30} y1={height-yScale(item)-40} x2={40} y2={height-yScale(item)-40} stroke="black" />
-                    <text
-                        x="10"
-                        y={height-yScale(item)-40}
-                        fontSize="10"
-                        textAnchor="middle"
-                        fill="black"
-                        strokeWidth="3">{item}</text>
-                </>
-            )
-        })}
-        <line x1="40" y1={0} x2="40" y2={height-40} stroke="black" />
-        <line x1="40" y1={height-40} x2={width} y2={height-40} stroke="black" />
 
-        {data.map((item,i)=>{
-            return (
-                <circle cx={xScale(item["sepalLength"])+40} cy={height-40-yScale(item["sepalWidth"])} r="5" fill={COLO(col,item.species,color) } />
-            )
-        })}
-        
 
-    </svg>
+
+    return (
+        <>
+            <select onChange={handleChangex} value={typex}>
+                {types.map((item) => {
+                    return (<option key={item} >{item}</option>)
+                })}
+
+            </select>
+            <select onChange={handleChangey} value={typey}>
+                {types.map((item) => {
+                    return (<option key={item}>{item}</option>)
+                })}
+
+            </select>
+            <svg width={width + 100} height={height + 40}>
+                {X.map((item) => {
+                    return (
+                        <>
+                            <line x1={xScale(item) + 40} y1={height} x2={xScale(item) + 40} y2={height + 10} stroke="black" />
+                            <text
+                                x={xScale(item) + 40}
+                                y={height + 20}
+                                fontSize="10"
+                                textAnchor="middle"
+                                fill="black"
+                                strokeWidth="3">{item}</text>
+                        </>
+                    )
+                })}
+                {Y.map((item) => {
+                    return (
+                        <>
+                            <line x1={30} y1={height - yScale(item)} x2={40} y2={height - yScale(item)} stroke="black" />
+                            <text
+                                x="10"
+                                y={height - yScale(item)}
+                                fontSize="10"
+                                textAnchor="left"
+                                fill="black"
+                                strokeWidth="3">{item}</text>
+                        </>
+                    )
+                })}
+                <line x1="40" y1={40} x2="40" y2={height} stroke="black" />
+                <line x1="40" y1={height} x2={width} y2={height} stroke="black" />
+
+                {dataM.map((item, i) => {
+                    return (
+                        <circle cx={xScale(item[typex]) + 40} cy={height - yScale(item[typey])} r="5" fill={COLO(col, item.species, color)} />
+                    )
+                })}
+
+                {col.map((item, i) => {
+                    return (
+                        <g onClick={() => handleClick(i, count)}>
+                            <rect key={item} x={width + 1} y={40 + i * 15} width="10" height="10" fill={color[i]} />
+                            <text x={width + 15} y={40 + i * 15 + 10} fontSize="13" textAnchor="left" fill="black" strokeWidth="3" >{item}</text>
+
+                        </g>)
+                })}
+            </svg>
+        </>
     )
 
 }
